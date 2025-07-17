04c05b1f6ec04bceb237c36244b30746669fffeb
Pitam <pitampoudelsaipu@gmail.com>
2025-07-17 10:58:05 +0545

    default auth url

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.Inet4Address
+import java.net.NetworkInterface
 import java.util.Properties
 
 plugins {
@@ -5,6 +7,21 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
+fun getLocalIpAddress(): String? {
+    val networkInterfaces = NetworkInterface.getNetworkInterfaces()
+    while (networkInterfaces.hasMoreElements()) {
+        val networkInterface = networkInterfaces.nextElement()
+        val inetAddresses = networkInterface.inetAddresses
+        while (inetAddresses.hasMoreElements()) {
+            val inetAddress = inetAddresses.nextElement()
+            if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                return "${inetAddress.hostAddress}"
+            }
+        }
+    }
+    return null
+}
+
 val propsFile = file("public.properties")
 val props = Properties()
 if (propsFile.exists()) {
@@ -14,3 +31,4 @@ if (propsFile.exists()) {
 props.forEach {
     extra[it.key.toString()] = it.value
 }
+if (!extra.has("AUTH_URL")) extra["AUTH_URL"] = "http://" + getLocalIpAddress() + ":8080"
