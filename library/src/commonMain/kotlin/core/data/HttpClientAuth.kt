e00c5b948a7d4393d0dbc02b7bb88506d9536fb3
Pitam <pitampoudelsaipu@gmail.com>
2025-07-17 19:19:29 +0545

    http client auth

---
+++ library/src/commonMain/kotlin/core/data/HttpClientAuth.kt
@@ -0,0 +1,39 @@
+package com.vardansoft.auth.core.data
+
+import com.vardansoft.auth.login.domain.LoginPreferences
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.auth.AuthConfig
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.core.scope.Scope
+
+fun AuthConfig.vardanSoftBearer(scope: Scope, clientId: String) {
+    val loginPreferences = scope.getOrNull<LoginPreferences>()
+
+    return bearer {
+        loadTokens {
+            loginPreferences?.oAuth2TokenData()?.let {
+                BearerTokens(
+                    accessToken = it.accessToken,
+                    refreshToken = it.refreshToken
+                )
+            }
+        }
+
+        refreshTokens {
+            tryTokenRefresh(
+                clientId = clientId,
+                client = HttpClient {
+                    install(ContentNegotiation) {
+                        json(Json {})
+                    }
+                },
+                loginPreferences = loginPreferences
+            )
+        }
+    }
+
+}
