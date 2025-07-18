ac35475ec5770387cdec93a0c179bcfaa75eed50
Pitam <pitampoudelsaipu@gmail.com>
2025-07-18 10:07:21 +0545

    missing actual

---
+++ ui/src/nativeMain/kotlin/com/vardansoft/auth/ui/core/components/CountryPicker.native.kt
@@ -0,0 +1,10 @@
+package com.vardansoft.auth.ui.core.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun CountryPicker(
+    countryNameCode: String,
+    onCountryNameCodeChanged: (String) -> Unit
+) {
+}
---
+++ ui/src/nativeMain/kotlin/com/vardansoft/auth/ui/otp/SmsOtpRetriever.native.kt
@@ -0,0 +1,8 @@
+package com.vardansoft.auth.ui.otp
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun registerSmsOtpRetriever(onRetrieved: (String) -> Unit): Boolean? {
+    TODO("Not yet implemented")
+}
