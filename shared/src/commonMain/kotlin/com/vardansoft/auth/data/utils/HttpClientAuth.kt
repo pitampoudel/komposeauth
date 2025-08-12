26731bfdc44135f85cbaf424f922590aeda33365
Pitam <pitampoudelsaipu@gmail.com>
2025-08-12 12:04:03 +0545

    send without request on ip

--- shared/src/commonMain/kotlin/com/vardansoft/auth/data/utils/HttpClientAuth.kt
+++ shared/src/commonMain/kotlin/com/vardansoft/auth/data/utils/HttpClientAuth.kt
@@ -35,8 +35,20 @@ fun AuthConfig.applyVardanSoftBearer(scope: Scope, clientId: String) {
             )
         }
         sendWithoutRequest {
-            it.url.host.contains("vardansoft.com")
+            val host = it.url.host
+            host.endsWith("vardansoft.com") || isIpAddress(host)
         }
     }
 
+}
+
+private fun isIpAddress(host: String): Boolean {
+    return isIPv4(host)
+}
+
+private fun isIPv4(host: String): Boolean {
+    val ipv4Regex = Regex(
+        "^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$"
+    )
+    return ipv4Regex.matches(host)
 }
