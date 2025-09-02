2bfb9c4a7aa02b3c9e606cd05aa3e6c439569ae9
Pitam <pitampoudelsaipu@gmail.com>
2025-09-02 17:47:14 +0545

    no credentials db

--- server/src/main/kotlin/com/vardansoft/authx/core/config/AuthSuccessHandler.kt
+++ server/src/main/kotlin/com/vardansoft/authx/core/config/AuthSuccessHandler.kt
@@ -1,8 +1,5 @@
 package com.vardansoft.authx.core.config
 
-import com.vardansoft.authx.credentials.dto.UpdateCredentialRequest
-import com.vardansoft.authx.credentials.entity.Credential
-import com.vardansoft.authx.credentials.service.CredentialService
 import com.vardansoft.authx.user.entity.User
 import com.vardansoft.authx.user.service.UserService
 import jakarta.servlet.http.HttpServletRequest
@@ -10,9 +7,6 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
@@ -20,9 +14,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class AuthSuccessHandler(
-    val userService: UserService,
-    val authorizedClientService: OAuth2AuthorizedClientService,
-    val credentialService: CredentialService
+    val userService: UserService
 ) : AuthenticationSuccessHandler {
 
     override fun onAuthenticationSuccess(
@@ -37,25 +29,10 @@ class AuthSuccessHandler(
                 val firstName: String = principal.getAttribute<String>("given_name").orEmpty()
                 val lastName: String = principal.getAttribute<String>("family_name").orEmpty()
                 val picture: String = principal.getAttribute<String>("picture").orEmpty()
-                val emailVerified: Boolean = principal.getAttribute<Boolean>("emailVerified") == true
+                val emailVerified: Boolean =
+                    principal.getAttribute<Boolean>("emailVerified") == true
                 user = userService.findOrCreateUserByEmail(email, firstName, lastName, picture)
                 if (emailVerified) userService.emailVerified(user.id)
-
-                (authentication as OAuth2AuthenticationToken).let { oAuth2Token ->
-                    authorizedClientService.loadAuthorizedClient<OAuth2AuthorizedClient>(
-                        oAuth2Token.authorizedClientRegistrationId,
-                        oAuth2Token.name
-                    ).let { oAuth2AuthorizedClient ->
-                        credentialService.updateCredential(
-                            userId = user.id,
-                            req = UpdateCredentialRequest(
-                                provider = Credential.Provider.GOOGLE,
-                                accessToken = oAuth2AuthorizedClient.accessToken.tokenValue,
-                                refreshToken = oAuth2AuthorizedClient.refreshToken?.tokenValue
-                            )
-                        )
-                    }
-                }
             }
 
             is UserDetails -> {
--- server/src/main/kotlin/com/vardansoft/authx/credentials/dto/CredentialResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.vardansoft.authx.credentials.dto
-
-import com.vardansoft.authx.credentials.entity.Credential
-
-data class CredentialResponse(
-    val provider: Credential.Provider,
-    val accessToken: String,
-    val refreshToken: String?
-)
