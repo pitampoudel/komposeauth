f3bc5d8a205de59a2d7f42e61b9769f80a14d500
Pitam <pitampoudelsaipu@gmail.com>
2025-09-03 13:44:19 +0545

    kmp file

---
+++ shared/src/commonMain/kotlin/com/vardansoft/core/data/EncodedData.kt
@@ -0,0 +1,16 @@
+package com.vardansoft.core.data
+
+import com.vardansoft.core.domain.KmpFile
+import io.ktor.util.decodeBase64Bytes
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class EncodedData(
+    @SerialName("data")
+    val data: String,
+    @SerialName("mime_type")
+    val mimeType: String?
+) {
+    fun toKmpFile() = KmpFile(data.decodeBase64Bytes(), mimeType)
+}
---
+++ shared/src/commonMain/kotlin/com/vardansoft/core/domain/KmpFile.kt
@@ -0,0 +1,24 @@
+package com.vardansoft.core.domain
+
+import com.vardansoft.core.data.EncodedData
+import io.ktor.util.encodeBase64
+
+data class KmpFile(val byteArray: ByteArray, val mimeType: String? = null) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as KmpFile
+
+        return byteArray.contentEquals(other.byteArray)
+    }
+
+    override fun hashCode(): Int {
+        return byteArray.contentHashCode()
+    }
+
+    fun toEncodedData() = EncodedData(
+        data = byteArray.encodeBase64(),
+        mimeType = mimeType
+    )
+}
