989585d2cb0a568288f524255ec9ae1ffbb5e93b
Pitam <pitampoudelsaipu@gmail.com>
2025-09-17 13:59:21 +0545

    build fix

--- shared/src/iosMain/kotlin/com/vardansoft/core/domain/DateTimeUtils.ios.kt
+++ shared/src/iosMain/kotlin/com/vardansoft/core/domain/DateTimeUtils.ios.kt
@@ -3,27 +3,15 @@ package com.vardansoft.core.domain
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
-import platform.Foundation.NSDateFormatter
-import platform.Foundation.NSDateFormatterMediumStyle
-import platform.Foundation.NSDateFormatterShortStyle
 
 actual fun LocalDate.asDisplayDate(): String {
-    val formatter = NSDateFormatter()
-    formatter.dateStyle = NSDateFormatterMediumStyle
-    formatter.timeStyle = NSDateFormatterShortStyle
-    return formatter.stringFromDate(this.toNSDate())
+    return "${dayOfMonth}-${monthNumber}-${year}"
 }
 
 actual fun LocalDateTime.asDisplayDateTime(): String {
-    val formatter = NSDateFormatter()
-    formatter.dateStyle = NSDateFormatterMediumStyle
-    formatter.timeStyle = NSDateFormatterShortStyle
-    return formatter.stringFromDate(this.toNSDate())
+    return "${dayOfMonth}-${monthNumber}-${year} ${hour}:${minute}:${second}"
 }
 
 actual fun LocalTime.asDisplayTime(): String {
-    val formatter = NSDateFormatter()
-    formatter.dateStyle = NSDateFormatterShortStyle
-    formatter.timeStyle = NSDateFormatterMediumStyle
-    return formatter.stringFromDate(this.toNSDate())
+    return "${hour}:${minute}:${second}"
 }
