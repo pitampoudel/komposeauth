be89b839e954a3415810a20d6762f14c54c653d4
Pitam <pitampoudelsaipu@gmail.com>
2025-09-25 09:00:56 +0545

    serialize fix

--- server/src/main/kotlin/com/vardansoft/authx/oauth_clients/dto/CreateClientRequest.kt
+++ server/src/main/kotlin/com/vardansoft/authx/oauth_clients/dto/CreateClientRequest.kt
@@ -1,9 +1,11 @@
 package com.vardansoft.authx.oauth_clients.dto
 
+import kotlinx.serialization.Serializable
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 
 val GOOGLE_ID_TOKEN_GRANT_TYPE = AuthorizationGrantType("urn:ietf:params:oauth:grant-type:google_id_token")
 
+@Serializable
 data class CreateClientRequest(
     val clientId: String? = null,
     val clientName: String,
