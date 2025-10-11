edf4d91e2c2009a4f7177184b87483aa84ac1895
Pitam <pitampoudelsaipu@gmail.com>
2025-10-11 19:05:47 +0545

    kmp file extension

--- shared/src/commonMain/kotlin/com/vardansoft/core/domain/KmpFile.kt
+++ shared/src/commonMain/kotlin/com/vardansoft/core/domain/KmpFile.kt
@@ -4,6 +4,8 @@ import com.vardansoft.core.data.EncodedData
 import io.ktor.util.encodeBase64
 
 data class KmpFile(val byteArray: ByteArray, val mimeType: String, val name: String) {
+    fun extension() = name.substringAfterLast('.', "").let { ext -> if (ext.isNotBlank()) ".$ext" else "" }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || this::class != other::class) return false
