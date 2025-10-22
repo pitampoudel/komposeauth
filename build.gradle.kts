7f06bc162b982ea6cfc571046d79f15574185ae7
Pitam <pitampoudelsaipu@gmail.com>
2025-10-22 08:29:13 +0545

    snapshot and release publishing workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import org.gradle.plugins.signing.Sign
+
 plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
@@ -6,6 +9,7 @@ plugins {
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
+
 buildscript {
     configurations.all {
         resolutionStrategy {
@@ -18,43 +22,39 @@ group = (findProperty("group") as String)
 version = (findProperty("version") as String)
 
 subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("com.vanniktech.maven.publish")) {
-            configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
-                publishToMavenCentral()
-                signAllPublications()
+    plugins.withId("com.vanniktech.maven.publish") {
+        configure<MavenPublishBaseExtension> {
+            publishToMavenCentral(true)
+            signAllPublications()
 
-                pom {
-                    inceptionYear = "2025"
+            pom {
+                inceptionYear = "2025"
+                url = "https://github.com/pitampoudel/komposeauth"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "pitampoudel"
+                        name = "Pitam Poudel"
+                        url = "https://www.pitam.com.np/"
+                    }
+                }
+                scm {
                     url = "https://github.com/pitampoudel/komposeauth"
-                    licenses {
-                        license {
-                            name = "The Apache License, Version 2.0"
-                            url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                            distribution = "repo"
-                        }
-                    }
-                    developers {
-                        developer {
-                            id = "pitampoudel"
-                            name = "Pitam Poudel"
-                            url = "https://www.pitam.com.np/"
-                        }
-                    }
-                    scm {
-                        url = "https://github.com/pitampoudel/komposeauth"
-                        connection = "scm:git:https://github.com/pitampoudel/komposeauth.git"
-                        developerConnection = "scm:git:ssh://git@github.com:pitampoudel/komposeauth.git"
-                    }
+                    connection = "scm:git:https://github.com/pitampoudel/komposeauth.git"
+                    developerConnection = "scm:git:ssh://git@github.com:pitampoudel/komposeauth.git"
                 }
             }
+        }
 
-            // Disable signing for local publishing
-            if (gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal") }) {
-                tasks.withType<Sign>().configureEach {
-                    enabled = false
-                }
-            }
+        // Disable signing for local publishing
+        tasks.withType<Sign>().configureEach {
+            onlyIf { !gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal") } }
         }
     }
 }
