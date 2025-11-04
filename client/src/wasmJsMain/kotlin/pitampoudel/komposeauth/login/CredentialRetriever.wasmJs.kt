d218d9c46ae04c6ce1a9f228eaa2189506814552
Pitam <pitampoudelsaipu@gmail.com>
2025-11-04 14:56:31 +0545

    Use JsFun for passkey creation

--- client/src/wasmJsMain/kotlin/pitampoudel/komposeauth/login/CredentialRetriever.wasmJs.kt
+++ client/src/wasmJsMain/kotlin/pitampoudel/komposeauth/login/CredentialRetriever.wasmJs.kt
@@ -26,6 +26,53 @@ import kotlin.js.Promise
 import kotlin.random.Random
 
 @OptIn(ExperimentalWasmJsInterop::class)
+@JsFun(
+    """
+    function base64urlToArrayBuffer(base64url) {
+      const base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
+      const pad = '='.repeat((4 - (base64.length % 4)) % 4);
+      const str = atob(base64 + pad);
+      const bytes = new Uint8Array(str.length);
+      for (let i = 0; i < str.length; i++) bytes[i] = str.charCodeAt(i);
+      return bytes.buffer;
+    }
+    function arrayBufferToBase64url(buffer) {
+      const bytes = new Uint8Array(buffer);
+      let binary = '';
+      for (let i = 0; i < bytes.byteLength; i++) binary += String.fromCharCode(bytes[i]);
+      return btoa(binary)
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+${'$'}/g, '');
+    }
+
+    async (optionsJson) => {
+      'use strict';
+        const options = JSON.parse(optionsJson);
+        // Convert binary fields from Base64URL to ArrayBuffers for WebAuthn API
+        options.challenge = base64urlToArrayBuffer(options.challenge);
+        options.user.id = base64urlToArrayBuffer(options.user.id);
+
+        const credential = await navigator.credentials.create({ publicKey: options });
+
+        // Convert back from ArrayBuffers → Base64URL strings for server
+        return {
+          publicKey: {
+            credential: {
+              id: credential.id,
+              rawId: arrayBufferToBase64url(credential.rawId),
+              type: credential.type,
+              response: {
+                attestationObject: arrayBufferToBase64url(credential.response.attestationObject),
+                clientDataJSON: arrayBufferToBase64url(credential.response.clientDataJSON)
+              }
+            },
+            label: "1password"
+          }
+        };
+       }
+    """
+)
 external fun createPasskey(optionsJson: String): Promise<JsAny>
 
 private fun generateCodeVerifier(length: Int = 64): String {
