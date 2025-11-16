ff3492876b2489e7333214e32211f2d3ec50395d
Pitam <pitampoudelsaipu@gmail.com>
2025-11-16 13:10:54 +0545

    feat: Add key based authorization to setup endpoints

--- server/src/main/kotlin/pitampoudel/komposeauth/setup/controller/SetupController.kt
+++ server/src/main/kotlin/pitampoudel/komposeauth/setup/controller/SetupController.kt
@@ -1,25 +1,35 @@
 package pitampoudel.komposeauth.setup.controller
 
-import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import pitampoudel.komposeauth.StaticAppProperties
 import pitampoudel.komposeauth.setup.entity.Env
 import pitampoudel.komposeauth.setup.service.EnvService
 
 @Controller
-@PreAuthorize("hasRole('SUPER_ADMIN') or @setupAccess.isOpen()")
-class SetupController(private val envService: EnvService) {
+class SetupController(
+    private val envService: EnvService,
+    private val appProps: StaticAppProperties,
+) {
     @GetMapping("/setup")
-    fun setupForm(model: Model): String {
+    fun setupForm(model: Model, @RequestParam("key") key: String?): String {
+        if (key != appProps.base64EncryptionKey) {
+            throw AccessDeniedException("You are not authorized")
+        }
         model.addAttribute("config", envService.getEnv())
         return "setup"
     }
 
     @PostMapping("/setup")
-    fun submit(@ModelAttribute form: Env, model: Model): String {
+    fun submit(@RequestParam("key") key: String?, @ModelAttribute form: Env, model: Model): String {
+        if (key != appProps.base64EncryptionKey) {
+            throw AccessDeniedException("You are not authorized")
+        }
         envService.save(form)
         envService.clearCache()
         model.addAttribute("config", envService.getEnv())
