357c6789aa724e8bb02d8374a2c4d9bdd058a243
Pitam <pitampoudelsaipu@gmail.com>
2025-11-19 13:36:35 +0545

    feat: Refresh SMTP settings for each email

--- server/src/main/kotlin/pitampoudel/komposeauth/core/service/EmailService.kt
+++ server/src/main/kotlin/pitampoudel/komposeauth/core/service/EmailService.kt
@@ -1,24 +1,39 @@
 package pitampoudel.komposeauth.core.service
 
-import org.springframework.beans.factory.ObjectProvider
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.stereotype.Service
+import pitampoudel.komposeauth.AppProperties
 
 @Service
-class EmailService(private val mailSenderProvider: ObjectProvider<JavaMailSender>) {
+class EmailService(private val appProperties: AppProperties) {
+
+    fun javaMailSender(): JavaMailSender {
+        val impl = JavaMailSenderImpl()
+        impl.host = appProperties.smtpHost
+        impl.port = appProperties.smtpPort ?: 587
+        impl.username = appProperties.smtpUsername
+        impl.password = appProperties.smtpPassword
+
+        val props = impl.javaMailProperties
+        props["mail.smtp.from"] = appProperties.smtpFromEmail
+        props["mail.smtp.auth"] = !appProperties.smtpUsername.isNullOrBlank()
+        props["mail.smtp.starttls.enable"] = "true"
+        return impl
+    }
+
     fun sendSimpleMail(
         to: String,
         subject: String? = null,
         text: String? = null
     ): Boolean {
-        val javaMailSender = mailSenderProvider.getIfAvailable() ?: return false
         return try {
             val mailMessage = SimpleMailMessage()
             mailMessage.setTo(to)
             mailMessage.text = text
             mailMessage.subject = subject
-            javaMailSender.send(mailMessage)
+            javaMailSender().send(mailMessage)
             true
         } catch (e: Exception) {
             false
