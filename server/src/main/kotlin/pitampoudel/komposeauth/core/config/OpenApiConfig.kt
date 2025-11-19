a00ba7bfdf40599375b74a1bbf3ccd95797a12d0
Pitam <pitampoudelsaipu@gmail.com>
2025-11-19 15:33:15 +0545

    feat: Manually add non-MVC endpoints to OpenAPI spec

--- server/src/main/kotlin/pitampoudel/komposeauth/core/config/OpenApiConfig.kt
+++ server/src/main/kotlin/pitampoudel/komposeauth/core/config/OpenApiConfig.kt
@@ -3,10 +3,18 @@ package pitampoudel.komposeauth.core.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.media.Content
+import io.swagger.v3.oas.models.media.MediaType
+import io.swagger.v3.oas.models.media.ObjectSchema
 import io.swagger.v3.oas.models.media.StringSchema
+import io.swagger.v3.oas.models.responses.ApiResponse
+import io.swagger.v3.oas.models.responses.ApiResponses
+import io.swagger.v3.oas.models.PathItem
+import io.swagger.v3.oas.models.Operation
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import kotlinx.datetime.LocalDate
+import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springdoc.core.utils.SpringDocUtils
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -52,5 +60,27 @@ class OpenApiConfig {
                             .bearerFormat("JWT")
                     )
             )
+    }
+
+    /**
+     * Manually document non-MVC endpoints that are served by filters or static handlers.
+     * SpringDoc scans only controller/functional endpoints, so we add those paths here.
+     */
+    @Bean
+    fun webauthnOpenApiCustomizer(): OpenApiCustomizer = OpenApiCustomizer { openApi: OpenAPI ->
+        openApi.path(
+            "/countries.json", PathItem().get(
+                Operation()
+                    .summary("Get countries")
+                    .description("Returns a list of countries")
+            )
+        )
+        openApi.path(
+            "/webauthn/register/options", PathItem().get(
+                Operation()
+                    .summary("Get WebAuthn register options")
+                    .description("Returns PublicKeyCredentialCreationOptions for WebAuthn registration")
+            )
+        )
     }
 }
