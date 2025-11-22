16fef9868ac472225e771ae936999f60f800ce18
Pitam <pitampoudelsaipu@gmail.com>
2025-11-22 22:57:34 +0545

    feat: Change webauthn registration options endpoint to POST

--- server/src/main/kotlin/pitampoudel/komposeauth/webauthn/controller/WebAuthnController.kt
+++ server/src/main/kotlin/pitampoudel/komposeauth/webauthn/controller/WebAuthnController.kt
@@ -12,6 +12,7 @@ import org.springframework.security.web.webauthn.management.ImmutablePublicKeyCr
 import org.springframework.security.web.webauthn.management.WebAuthnRelyingPartyOperations
 import org.springframework.security.web.webauthn.registration.PublicKeyCredentialCreationOptionsRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import pitampoudel.komposeauth.app_config.service.AppConfigProvider
@@ -58,7 +59,7 @@ class WebAuthnController(
         description = "Returns PublicKeyCredentialCreationOptions for WebAuthn registration",
         tags = ["webauthn"]
     )
-    @GetMapping("/webauthn/register/options")
+    @PostMapping("/webauthn/register/options")
     fun getRegisterOptions(
         authentication: Authentication,
         request: HttpServletRequest,
