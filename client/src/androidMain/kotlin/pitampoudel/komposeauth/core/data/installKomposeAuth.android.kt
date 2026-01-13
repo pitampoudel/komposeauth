9d4315657fd2a150be55960216e5ea336eb05c0a
Pitam <pitampoudelsaipu@gmail.com>
2026-01-13 21:45:24 +0545

    feat: Provide observable SharedPreferences settings to AuthPreferences on Android

--- client/src/androidMain/kotlin/pitampoudel/komposeauth/core/data/installKomposeAuth.android.kt
+++ client/src/androidMain/kotlin/pitampoudel/komposeauth/core/data/installKomposeAuth.android.kt
@@ -1,14 +1,24 @@
 package pitampoudel.komposeauth.core.data
 
 import android.content.Context
+import com.russhwolf.settings.Settings
+import com.russhwolf.settings.SharedPreferencesSettings
+import com.russhwolf.settings.observable.makeObservable
 import io.ktor.client.HttpClientConfig
 import pitampoudel.komposeauth.login.data.AuthPreferencesImpl
+
+fun createSettings(context: Context): Settings {
+    val sp = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
+    return SharedPreferencesSettings(sp)
+}
 
 fun HttpClientConfig<*>.installKomposeAuth(
     context: Context,
     authServerUrl: String,
     resourceServerUrls: List<String>
 ) {
-    val authPreferences = AuthPreferencesImpl.getInstance()
+    val authPreferences = AuthPreferencesImpl.getInstance(
+        createSettings(context).makeObservable()
+    )
     installKomposeAuth(authPreferences, authServerUrl, resourceServerUrls)
 }
