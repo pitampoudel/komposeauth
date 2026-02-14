5e98628c69d0186416ef956b34962a39bd78d659
Pitam <pitampoudelsaipu@gmail.com>
2026-02-14 12:08:02 +0545

    refactor: replace LinearProgressIndicator with CircularProgressIndicator in ProgressDialog

--- client/src/commonMain/kotlin/pitampoudel/core/presentation/wrapper/screenstate/ProgressDialog.kt
+++ client/src/commonMain/kotlin/pitampoudel/core/presentation/wrapper/screenstate/ProgressDialog.kt
@@ -1,25 +1,20 @@
 package pitampoudel.core.presentation.wrapper.screenstate
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.window.Dialog
 
 @Composable
 fun ProgressDialog(progress: Float, logo: Painter?, onDismissProgress: (() -> Unit)?) {
-    if (progress != 0F) {
-        Dialog(onDismissRequest = { onDismissProgress?.invoke() }) {
-        }
-        LinearProgressIndicator(
-            progress = {
-                progress
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
-    } else {
-        Dialog(onDismissRequest = { onDismissProgress?.invoke() }) {
+    Dialog(onDismissRequest = { onDismissProgress?.invoke() }) {
+        if (progress != 0F) {
+            CircularProgressIndicator(
+                progress = {
+                    progress
+                }
+            )
+        } else {
             logo?.let {
                 LogoLoadingAnimation(logo = logo)
             } ?: run {
@@ -28,4 +23,3 @@ fun ProgressDialog(progress: Float, logo: Painter?, onDismissProgress: (() -> Un
         }
     }
 }
-
