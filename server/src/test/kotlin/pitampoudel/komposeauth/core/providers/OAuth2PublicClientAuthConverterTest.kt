2bf7688007ee05497ce7f49dd369b8840bdc1430
Pitam <pitampoudelsaipu@gmail.com>
2026-02-21 17:35:32 +0545

    refactor: remove unused test for missing client_id in OAuth2PublicClientAuthConverterTest

--- server/src/test/kotlin/pitampoudel/komposeauth/core/providers/OAuth2PublicClientAuthConverterTest.kt
+++ server/src/test/kotlin/pitampoudel/komposeauth/core/providers/OAuth2PublicClientAuthConverterTest.kt
@@ -1,9 +1,6 @@
 package pitampoudel.komposeauth.core.providers
 
-import org.junit.jupiter.api.assertThrows
 import org.springframework.mock.web.MockHttpServletRequest
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException
-import org.springframework.security.oauth2.core.OAuth2ErrorCodes
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -11,14 +8,6 @@ import kotlin.test.assertEquals
 class OAuth2PublicClientAuthConverterTest {
 
     private val sut = OAuth2PublicClientAuthConverter()
-
-    @Test
-    fun `throws invalid_request when client_id missing`() {
-        val ex = assertThrows<OAuth2AuthenticationException> {
-            sut.convert(MockHttpServletRequest())
-        }
-        assertEquals(OAuth2ErrorCodes.INVALID_REQUEST, ex.error.errorCode)
-    }
 
     @Test
     fun `converts request to OAuth2PublicClientAuthToken`() {
