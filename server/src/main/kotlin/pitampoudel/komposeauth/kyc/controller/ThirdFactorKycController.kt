dbf0c5d14ee54fe400498a97cbc739bdd2170ab8
Pitam <pitampoudelsaipu@gmail.com>
2026-03-09 12:40:59 +0545

    refactor: inline JWT token generation in third-factor KYC request body

--- server/src/main/kotlin/pitampoudel/komposeauth/kyc/controller/ThirdFactorKycController.kt
+++ server/src/main/kotlin/pitampoudel/komposeauth/kyc/controller/ThirdFactorKycController.kt
@@ -40,25 +40,28 @@ class ThirdFactorKycController(
             ?: error("Third-factor token is not configured")
         val thirdFactorUrl = appConfigService.getConfig().thirdFactorUrl?.takeIf { it.isNotBlank() }
             ?: error("Third-factor URL is not configured")
-        val jwt = jwtTokenService.generateHs256Token(
-            secretKey = secretKey,
-            subject = user.id.toHexString(),
-            issuer = findServerUrl(httpServletRequest),
-            claims = mapOf(
-                "name" to user.fullName,
-                "token" to token,
-                "identifier" to user.id.toHexString(),
-                "label" to "",
-                "secondary_label" to "",
-                "callback" to findServerUrl(httpServletRequest) + "/$THIRD_FACTOR_KYC",
-                "is_sdk" to "true"
-            )
-        )
         val response = restClient.post()
             .uri("$thirdFactorUrl/tfauth/get-kyc-url/")
             .header("Authorization", "Bearer $token")
             .contentType(MediaType.APPLICATION_JSON)
-            .body(mapOf("jwt" to jwt))
+            .body(
+                mapOf(
+                    "jwt" to jwtTokenService.generateHs256Token(
+                        secretKey = secretKey,
+                        subject = user.id.toHexString(),
+                        issuer = findServerUrl(httpServletRequest),
+                        claims = mapOf(
+                            "name" to user.fullName,
+                            "token" to token,
+                            "identifier" to user.id.toHexString(),
+                            "label" to "",
+                            "secondary_label" to "",
+                            "callback" to findServerUrl(httpServletRequest) + "/$THIRD_FACTOR_KYC",
+                            "is_sdk" to "true"
+                        )
+                    )
+                )
+            )
             .retrieve()
             .body<ThirdFactorKycUrlResponse>()
             ?: error("Third-factor KYC URL response was empty")
