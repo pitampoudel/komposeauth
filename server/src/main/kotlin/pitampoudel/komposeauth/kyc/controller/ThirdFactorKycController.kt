cede6c6b93a069650814bee5e26b6901e902a163
Pitam <pitampoudelsaipu@gmail.com>
2026-03-09 12:31:10 +0545

    feat: add third-factor KYC URL generation endpoint with JWT-based authentication

--- server/src/main/kotlin/pitampoudel/komposeauth/kyc/controller/ThirdFactorKycController.kt
+++ server/src/main/kotlin/pitampoudel/komposeauth/kyc/controller/ThirdFactorKycController.kt
@@ -1,31 +1,76 @@
 package pitampoudel.komposeauth.kyc.controller
 
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
+import org.springframework.web.servlet.view.RedirectView
+import pitampoudel.komposeauth.app_config.service.AppConfigService
+import pitampoudel.komposeauth.core.config.UserContextService
 import pitampoudel.komposeauth.core.domain.ApiEndpoints.THIRD_FACTOR_KYC
-import pitampoudel.komposeauth.core.service.EmailService
-import pitampoudel.komposeauth.core.service.SlackNotifier
+import pitampoudel.komposeauth.core.service.jwt.JwtTokenService
+import pitampoudel.komposeauth.core.utils.findServerUrl
+import pitampoudel.komposeauth.kyc.data.KycResponse
 import pitampoudel.komposeauth.kyc.dto.ThirdFactorModel
 import pitampoudel.komposeauth.kyc.service.KycService
 import pitampoudel.komposeauth.user.service.UserService
 
 @RestController
-
 class ThirdFactorKycController(
     val userService: UserService,
     private val kycService: KycService,
-    private val emailService: EmailService,
-    private val slackNotifier: SlackNotifier
+    val appConfigService: AppConfigService,
+    val jwtTokenService: JwtTokenService,
+    val userContextService: UserContextService,
+    val restClient: RestClient
 ) {
-    @PostMapping("/$THIRD_FACTOR_KYC")
-    fun submit(@Validated @RequestBody data: ThirdFactorModel): ResponseEntity<*> {
 
-
-        return ResponseEntity.ok("received successfully")
+    @GetMapping("/$THIRD_FACTOR_KYC")
+    fun generateUrl(httpServletRequest: HttpServletRequest): RedirectView {
+        val user = userContextService.getUserFromAuthentication()
+        val secretKey = appConfigService.getConfig().thirdFactorSecretKey
+            ?: error("Third-factor secret key is not configured")
+        val token = appConfigService.getConfig().thirdFactorToken
+            ?: error("Third-factor token is not configured")
+        val thirdFactorUrl = appConfigService.getConfig().thirdFactorUrl?.takeIf { it.isNotBlank() }
+            ?: error("Third-factor URL is not configured")
+        val jwt = jwtTokenService.generateHs256Token(
+            secretKey = secretKey,
+            subject = user.id.toHexString(),
+            issuer = findServerUrl(httpServletRequest),
+            claims = mapOf(
+                "name" to user.fullName,
+                "token" to token,
+                "identifier" to user.id.toHexString(),
+                "label" to "",
+                "secondary_label" to "",
+                "callback" to findServerUrl(httpServletRequest) + "/$THIRD_FACTOR_KYC",
+                "is_sdk" to "true"
+            )
+        )
+        val response = restClient.post()
+            .uri("$thirdFactorUrl/tfauth/get-kyc-url/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapOf("jwt" to jwt))
+            .retrieve()
+            .body<ThirdFactorKycUrlResponse>()
+            ?: error("Third-factor KYC URL response was empty")
+        return RedirectView(response.url)
     }
 
+    @PostMapping("/$THIRD_FACTOR_KYC")
+    fun submit(@Validated @RequestBody data: ThirdFactorModel): ResponseEntity<KycResponse> {
+        return ResponseEntity.ok(kycService.submitThirdFactorVerification(data))
+    }
+
+    data class ThirdFactorKycUrlResponse(
+        val url: String
+    )
 
 }
